ff09fb6e09a8b356fa8ca2dbe18beaa637bccb8c
Ash <ashleylstringer@gmail.com>
2025-07-18 22:05:35 -0400

    .

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,7 +5,7 @@ import 'dotenv/config'
 console.log(`WEBSITE_URL: ${process.env.WEBSITE_URL}`);
 
 test('has title', async ({ page }) => {
-  await page.goto(`https://${process.env.WEBSITE_URL}`);
+  await page.goto(`${process.env.WEBSITE_URL}`);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Resume Site/);
@@ -15,7 +15,7 @@ test('has title', async ({ page }) => {
 });
 
 test('has number in Vistor Count', async ({ page }) => {
-  await page.goto(`https://${process.env.WEBSITE_URL}`);
+  await page.goto(`${process.env.WEBSITE_URL}`);
 
   const visitorCount = page.locator(".visitor-count-display");
   //await expect(visitorCount).toHaveText(/Visitor Count: \d+/);
